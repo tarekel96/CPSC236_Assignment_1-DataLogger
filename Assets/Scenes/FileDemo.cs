d0d51b26452fd049f141f39a2850204cf3354e76
Tarek <elhaj102@mail.chapman.edu>
2021-02-10 15:26:25 -0800

    uploading to remote repo

---
+++ Assets/Scenes/FileDemo.cs
@@ -0,0 +1,80 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+namespace DataLogger
+{
+    public class FileDemo
+    {
+        /* Fields */
+        public enum LogType {OnStart, OnDestroy, SpaceBar, OnClick}
+        protected string m_outFileName;
+        StreamWriter m_outFile = null;
+        const string DEFAULT_FILE = "text.txt";
+        /* Constructor */
+        public FileDemo(string outFileName = "")
+        {
+            m_outFileName = outFileName;
+
+            if (outFileName == "")
+            {
+                m_outFileName = DEFAULT_FILE;
+            }
+
+            if(File.Exists(m_outFileName) == false)
+            {
+                File.Create(m_outFileName).Close();
+            }
+
+            m_outFile = new StreamWriter(m_outFileName);
+
+        }
+        /* Mutators */
+        public void AddToStrToFile(string message)
+        {
+            m_outFile.WriteLine(message);
+            
+        }
+        /* Accessors */
+        public string GetCurrentTime(LogType logType)
+        {
+            string TimeStampMsg = "";
+            switch (logType)
+            {
+                case LogType.OnStart:
+                    TimeStampMsg += "OnStart ~ ";
+                    break;
+                case LogType.OnDestroy:
+                    TimeStampMsg += "OnDestroy ~ ";
+                    break;
+                case LogType.SpaceBar:
+                    TimeStampMsg += "SpaceBar ~ ";
+                    break;
+                case LogType.OnClick:
+                    TimeStampMsg += "OnClick ~ ";
+                    break;
+                default:
+                    return "ERROR: Invalid Argument Input";
+            }
+            TimeStampMsg += "Time Stamp Message: " + DateTime.Now.ToString() + Environment.NewLine;
+            return TimeStampMsg;
+        }
+        /* Utility Functions */
+        public void LogCurrentTime(LogType logType)
+        {
+            Debug.Log(GetCurrentTime(logType));
+            AddToStrToFile(GetCurrentTime(logType));
+        }
+        public void HandleClick()
+        {
+            Debug.Log(GetCurrentTime(LogType.OnClick));
+            AddToStrToFile(GetCurrentTime(LogType.OnClick));
+        }
+        public void SaveAndCloseFile()
+        {
+            m_outFile.Close();
+        }
+    }
+}
---
+++ Assets/Scenes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+namespace DataLogger
+{
+    public class Program : MonoBehaviour
+    {
+        FileDemo FileHandler;
+        //const string FileName = "text.txt";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            FileHandler = new FileDemo();
+
+            /* Timestamp of when App first loads */
+            FileHandler.LogCurrentTime(FileDemo.LogType.OnStart);
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                FileHandler.LogCurrentTime(FileDemo.LogType.SpaceBar);
+            }
+
+        }
+
+        // OnDestroy called when program shuts down
+        void OnDestroy()
+        {
+            if(FileHandler != null)
+            {
+                /* Timestamp of when App is about to quit */
+                FileHandler.LogCurrentTime(FileDemo.LogType.OnDestroy);
+                FileHandler.SaveAndCloseFile();
+                Debug.Log("Check the file: " + Environment.CurrentDirectory + "/text.txt");
+            }
+        }
+        // Utility Functions
+        public void InvokeHandleClick()
+        {
+            FileHandler.HandleClick();
+        }
+    }
+}
